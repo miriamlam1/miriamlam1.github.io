6727f00fea779ab5569593cc4d75afd23d3dd741
Miriam <mlam27@calpoly.edu>
2020-07-23 02:51:51 -0700

    gotta save

--- file_organizer.py
+++ file_organizer.py
@@ -1,7 +1,32 @@
 import os
 import json
+from PIL import Image
+from pathlib import Path
 
-extlist = ["png","jpg","svg"]
+sorting_types = ["interface","branding","typography","other"]
+
+#from https://note.nkmk.me/en/python-pillow-image-crop-trimming/
+#crops image to its center
+def crop_center(pil_img, crop_width, crop_height):
+    img_width, img_height = pil_img.size
+    return pil_img.crop(((img_width - crop_width) // 2,
+        (img_height - crop_height) // 2,
+        (img_width + crop_width) // 2,
+        (img_height + crop_height) // 2))
+
+#from https://note.nkmk.me/en/python-pillow-image-crop-trimming/
+#crops the max square possible frm center
+def crop_max_square(pil_img):
+    return crop_center(pil_img, min(pil_img.size), min(pil_img.size))
+
+#compress the image for website
+def image_compress(path):
+    foo = Image.open(path)
+    foo = crop_max_square(foo)
+    foo = foo.resize((800,800),Image.ANTIALIAS)
+    index = (path.rfind('.'))
+    path = path[:index] + "_cover_" + path[index:]
+    foo.save(path,quality=98,optimize=True)
 
 def path_to_dict(path):
     new_json = {'name': os.path.basename(path)}
@@ -13,9 +38,11 @@ def path_to_dict(path):
         new_json['children'] = children_list
     else:
         if new_json["name"].endswith(".jpg") or new_json["name"].endswith(".png"):
+            if "_cover_" not in path: 
+                image_compress(path)
             new_json['type'] = "image"
         else:
             new_json['type'] = "file"
     return new_json
-
+#path_to_dict('.')
 print(json.dumps(path_to_dict('.')))
--- scripts/jsonparseimage.js
+++ scripts/jsonparseimage.js
@@ -15,27 +15,49 @@ function editHTML(object){
     var html = "";
     var i;
     for(i=0;i<object.length;i++){
-        if (object[i].type == "image"){
-            html += imageHTML(object[i].name,"");
+        if (object[i].name.includes("cover") && object[i].type == "image"){
+            var hashtag = object[i].name.split("_")[0];
+            html += imageHTML(object[i].name,"",hashtag);
         }
         else if(object[i].type == "directory"){
-            if (object[i].children[0].type == "image"){
-                console.log(object[i]);
-                html += imageHTML(object[i].children[0].name, object[i].name);
+            var j;
+            for(j=0;j<object[i].children.length;j++){
+                // look for type cover first then first _cover_ in list
+                if (object[i].children[j].type == "cover"){
+                    var hashtag = object[i].name.split("_")[0];
+                    html += imageHTML(object[i].children[j].name, object[i].name, hashtag);
+                    break; //dont display multiple of same folder
+                }
+            }
+            //if no cover types found then default to first in list
+            if (j==object[i].children.length){ 
+                for(j=0;j<object[i].children.length;j++){
+                    if (object[i].children[j].name.includes("cover")){
+                        var hashtag = object[i].name.split("_")[0];
+                        html += imageHTML(object[i].children[j].name, object[i].name, hashtag);
+                        break;
+                    }
+                }
             }
         }
     }
-    console.log(html);
+    //console.log(html);
     return html;
 }
 
-function imageHTML(object, dir){
-    var type = object.split("_")[0];
+function imageHTML(object, dir, type){
     var location = dir + "/"+ object;
     var project_link = object.split(".")[0];
     var design_name = object.split(".")[0];
 
-    var html = '<div class="item '+ type +'"><img src="lhea_images/'+ location +'">';
+    //caveat because i used interface and interface design interchangably
+    if (type.includes("interface")){
+        var aclass = "interface-design";
+    } else {
+        var aclass = type;
+    }
+
+    var html = '<div class="item '+ aclass +'"><img src="lhea_images/'+ location +'">';
     html += '<div class="overlay"><p><a href="'+ project_link +'.html">';
     html += design_name + '</a> <br> #'+ type +'</p></div></div>';
     return html;
